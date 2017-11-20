be23f9b93b94f7bd0f064dfe4fa53a8f2ed939ce
Abhinav <abhijim007@gmail.com>
2017-11-20 21:10:27 +0530

    Delete Lottery.py

--- Lottery.py
+++
@@ -1,7 +0,0 @@
->>> def lottery():
-	for i in range(6):
-		yield random.randint(1,40)
-	yield random.randint(1,15)
-
->>> for random_number in lottery():
-	print("And the next number is ... %d" %(random_number))
